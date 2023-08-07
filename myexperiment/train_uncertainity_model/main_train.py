d7b506d132543893c44f28cbd30242d2f2286bdc
DirtyBoy <49908353+Dirtyboy1029@users.noreply.github.com>
2023-08-07 15:24:32 +0800

    Update main_train.py

--- myexperiment/train_uncertainity_model/main_train.py
+++ myexperiment/train_uncertainity_model/main_train.py
@@ -24,11 +24,11 @@ if __name__ == '__main__':
         feature_type = "multimodality"
         model_dir = "multi_model/"
     if args.model_type == "big":
-        mal_folder = "/home/public/rmt/heren/experiment/cl-exp/LHD_apk/apk/train_dnn_model/big_dataset/malware_11354"
-        ben_folder = "/home/public/rmt/heren/experiment/cl-exp/LHD_apk/apk/train_dnn_model/big_dataset/benign_11316"
+        mal_folder = "/home/public/rmt/heren/experiment/cl-exp/LHD_apk/apk/train_dnn_model/big_dataset/malware"
+        ben_folder = "/home/public/rmt/heren/experiment/cl-exp/LHD_apk/apk/train_dnn_model/big_dataset/benign"
     else:
-        mal_folder = "/home/public/rmt/heren/experiment/cl-exp/LHD_apk/apk/train_dnn_model/small_dataset/malware_5794"
-        ben_folder = "/home/public/rmt/heren/experiment/cl-exp/LHD_apk/apk/train_dnn_model/small_dataset/benign_5758"
+        mal_folder = "/home/public/rmt/heren/experiment/cl-exp/LHD_apk/apk/train_dnn_model/small_dataset/malware"
+        ben_folder = "/home/public/rmt/heren/experiment/cl-exp/LHD_apk/apk/train_dnn_model/small_dataset/benign"
 
     train_dataset, test_dataset, test_y, input_dim = data_preprocessing(feature_type=feature_type,
                                                                         malware_dir=mal_folder,
