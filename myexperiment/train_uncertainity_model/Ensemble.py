4c5cf4dbbd20274f30ea336d604289b3576ef4dc
DirtyBoy <49908353+Dirtyboy1029@users.noreply.github.com>
2023-08-07 15:25:15 +0800

    Update Ensemble.py

--- myexperiment/train_uncertainity_model/Ensemble.py
+++ myexperiment/train_uncertainity_model/Ensemble.py
@@ -31,13 +31,7 @@ if __name__ == '__main__':
     train_dataset, test_dataset, input_dim = data_preprocessing(feature_type=feature_type, mal_folder=mal_folder,
                                                                 ben_folder=ben_folder, model_type=model_type)
 
-    d_ensemble1020 = DeepEnsemble(architecture_type=architecture_type, n_members=10,
-                                  model_directory="/home/lhd/uncertainity_malware/myexperiment/model/" + model_dir + model_type + "_model/ensemble/n_member10/epoch20")
-
-    d_ensemble1020.fit(train_set=train_dataset, input_dim=input_dim, EPOCH=20)
-
-    msg1020 = "1020--" + d_ensemble1020.evaluate(test_dataset)
-
+   
     d_ensemble1030 = DeepEnsemble(architecture_type=architecture_type, n_members=10,
                                   model_directory="/home/lhd/uncertainity_malware/myexperiment/model/" + model_dir + model_type + "_model/ensemble/n_member10/epoch30")
 
@@ -45,22 +39,7 @@ if __name__ == '__main__':
 
     msg1030 = "1030--" + d_ensemble1030.evaluate(test_dataset)
 
-    d_ensemble520 = DeepEnsemble(architecture_type=architecture_type, n_members=5,
-                                 model_directory="/home/lhd/uncertainity_malware/myexperiment/model/" + model_dir + model_type + "_model/ensemble/n_member5/epoch20")
-
-    d_ensemble520.fit(train_set=train_dataset, input_dim=input_dim, EPOCH=20)
-
-    msg520 = "520--" + d_ensemble520.evaluate(test_dataset)
-
-    d_ensemble530 = DeepEnsemble(architecture_type=architecture_type, n_members=5,
-                                 model_directory="/home/lhd/uncertainity_malware/myexperiment/model/" + model_dir + model_type + "_model/ensemble/n_member5/epoch30")
-
-    d_ensemble530.fit(train_set=train_dataset, input_dim=input_dim, EPOCH=30)
-
-    msg530 = "530--" + d_ensemble530.evaluate(test_dataset)
-
-    MSG = msg520 + "\n" + msg530 + "\n" + msg1030 + "\n" + msg1020
-
+   
     with open(
             '/home/lhd/uncertainity_malware/myexperiment/output/eval_model_log/' + model_dir + model_type + '_ensemble.txt',
             'w') as f:
