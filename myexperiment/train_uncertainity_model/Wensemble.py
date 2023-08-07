7ca697745669d94b86e7e683f8ad081f77b86cc1
DirtyBoy <49908353+Dirtyboy1029@users.noreply.github.com>
2023-08-07 15:26:18 +0800

    Update Wensemble.py

--- myexperiment/train_uncertainity_model/Wensemble.py
+++ myexperiment/train_uncertainity_model/Wensemble.py
@@ -30,27 +30,7 @@ if __name__ == '__main__':
     train_dataset, test_dataset, input_dim = data_preprocessing(feature_type=feature_type, mal_folder=mal_folder,
                                                                 ben_folder=ben_folder, model_type=model_type)
 
-    w_ensemble1020 = WeightedDeepEnsemble(architecture_type=architecture_type, n_members=10,
-                                          model_directory="/home/lhd/uncertainity_malware/myexperiment/model/" + model_dir + model_type + "_model/wensemble/n_member10/epoch20")
-
-    w_ensemble1020.fit(train_set=train_dataset, input_dim=input_dim, EPOCH=20)
-
-    msg1020 = "1020--" + w_ensemble1020.evaluate(test_dataset)
-
-    w_ensemble520 = WeightedDeepEnsemble(architecture_type=architecture_type, n_members=5,
-                                         model_directory="/home/lhd/uncertainity_malware/myexperiment/model/" + model_dir + model_type + "_model/wensemble/n_member5/epoch20")
-
-    w_ensemble520.fit(train_set=train_dataset, input_dim=input_dim, EPOCH=20)
-
-    msg520 = "520--" + w_ensemble520.evaluate(test_dataset)
-
-    w_ensemble530 = WeightedDeepEnsemble(architecture_type=architecture_type, n_members=5,
-                                         model_directory="/home/lhd/uncertainity_malware/myexperiment/model/" + model_dir + model_type + "_model/wensemble/n_member5/epoch30")
-
-    w_ensemble530.fit(train_set=train_dataset, input_dim=input_dim, EPOCH=30)
-
-    msg530 = "530--" + w_ensemble530.evaluate(test_dataset)
-
+   
     w_ensemble1030 = WeightedDeepEnsemble(architecture_type=architecture_type, n_members=10,
                                           model_directory="/home/lhd/uncertainity_malware/myexperiment/model/" + model_dir + model_type + "_model/wensemble/n_member10/epoch30")
 
@@ -58,7 +38,7 @@ if __name__ == '__main__':
 
     msg1030 = "1030--" + w_ensemble1030.evaluate(test_dataset)
 
-    MSG = msg520 + "\n" + msg530 + "\n" + msg1030 + "\n" + msg1020
+    MSG = msg1030 
 
     with open(
             '/home/lhd/uncertainity_malware/myexperiment/output/eval_model_log/' + model_dir + model_type + '_wensemble.txt',
